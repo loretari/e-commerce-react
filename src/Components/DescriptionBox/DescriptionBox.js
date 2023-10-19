86e4ac37892458a6bf3d559670df71250d3fa2dd
loretari <loretari@gmail.com>
2023-10-19 18:49:03 +0300

    init: add DescriptionBox

---
+++ src/Components/DescriptionBox/DescriptionBox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './DescriptionBox.css';
+
+const DescriptionBox = () => {
+    return (
+        <div className= "descriptionbox">
+<div className= "descriptionbox-navigator">
+    <div className= "descriptionbox-nav-box">Description</div>
+    <div className= "descriptionbox-nav-box fade">Reviews (122)</div>
+</div>
+          <div className= "descriptionbox-description">
+              <p>An e-commerce website is your digital storefront on the internet.
+                  It facilitates the transaction between a buyer and seller.
+                  It is the virtual space where you showcase products,
+                  and online customers make selections.
+                  Your website acts as the product shelves,
+                  sales staff, and cash register of your online business channel.</p>
+              <p>
+                  <p>An e-commerce website is your digital storefront on the internet.
+                      It facilitates the transaction between a buyer and seller.
+                      It is the virtual space where you showcase products,
+                      and online customers make selections.
+                      Your website acts as the product shelves,
+                      sales staff, and cash register of your online business channel.</p>
+              </p>
+          </div>
+        </div>
+    )
+}
+
+export default DescriptionBox;
